e03289ffd9f9ad21fc125556e17560bbb8b3327c
RinnarBetta <rinnar007@gmail.com>
2024-10-22 22:56:25 +0300

    New Commit

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -5,24 +5,15 @@ public class DataHandler {
     // Узагальнений нестатичний метод, який має параметр
     // як масив узагальненого типу та повертає String значення
     // обробленого масиву
-    public String handleData(String[] items) {
+    public <T> String handleData(T[] items) {
         StringBuilder sb = new StringBuilder();
         int count = 0;
         // Цикл обробки масиву узагальненого типу
-        for (String item : items) {
+        for (T item : items) {
             count++;
             sb.append(String.format("(%d) %s ", count, item));
         }
         return sb.toString();
     }
-    public String handleData(Double[] items) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        // Цикл обробки масиву узагальненого типу
-        for (Double item : items) {
-            count++;
-            sb.append(String.format("(%d) %s ", count, item));
-        }
-        return sb.toString();
-    }
+
 }
